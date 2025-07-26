041504dab12a073323cf3a5b9a12db3220de4a30
Krushnakant Patil <krushnakantpatil06@gmail.com>
2025-07-26 11:44:25 +0530

    commiting main files

---
+++ visualize.py
@@ -0,0 +1,22 @@
+import subprocess
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+
+file_path = 'radix_steps.csv'
+if os.path.exists(file_path):
+    os.remove(file_path)
+    open(file_path, 'w').close()  # Recreate an empty file
+
+subprocess.run(["./a.exe"])
+
+
+df = pd.read_csv("radix_steps.csv", header=None)
+
+for index, row in df.iterrows():
+    title = row[0]
+    data = row[1:-1]
+    plt.bar(range(len(data)), data)
+    plt.title(title)
+    plt.pause(.01)
+    plt.clf()
